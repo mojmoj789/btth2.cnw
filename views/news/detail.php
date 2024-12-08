f6cd851c10764b4749703bf317dc795920581aa7
vietdung8804 <145429537+vietdung8804@users.noreply.github.com>
2024-12-08 22:26:57 +0700

    update

--- views/news/detail.php
+++ views/news/detail.php
@@ -1,1 +1,199 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web Tin Tức - Khoa Học</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <div class="navbar">
+            <label class="left">Thứ 7 Ngày 7 tháng 12 năm 2024</label>
+            <label class="right">Đăng Nhập</label>
+        </div>
+        <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/vi/6/6b/Logo_trang_24h.png" style="max-height: 120px;">
+            <div class="search-form">
+                <input type="text" name="s" placeholder="Tìm Kiếm ...">
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div class="menu">
+            <ul>
+                <li>Trang Chủ</li>
+                <li>Thời Sự</li>
+                <li>Thể Thao</li>
+                <li>Thế Giới</li>
+                <li>Giải Trí</li>
+                <li>Khoa Học</li>
+                <li>Kinh Doanh</li>
+                <li>Du Lịch</li>
+                <li>Công Nghệ</li>
+                <li>Sức Khỏe</li>
+            </ul>
+        </div>
+    </div>
+    <div class="body-content">
+        <div class="main-content">
+            <!-- row: 12 cell -->
+            <div class="content">
+                <div class="row">
+                    <div class="col-4">
+                        <img src="https://i1-vnexpress.vnecdn.net/2024/12/07/466869431-28175020878755976-91-8638-8585-1733578424.png?w=680&h=0&q=100&dpr=1&fit=crop&s=cgUGP8-FQyM6-c18CbYp-w" style=" width: 100%">
+                    </div>
+                    <div class="col-4">
+                        <div class="content">
+                            <h3>Phiến quân 'cách thủ đô Syria 10 km'</h3>
+                                <p>Phiến quân ở Syria được cho là chỉ còn cách thủ đô Damascus khoảng 10 km, sau khi chiếm được tỉnh Daraa ở miền nam.</p>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="content">
+                            <ul>
+                                <li> Ông Trump: Mỹ không nên dính líu đến xung đột Syria</li>
+                                <li> Người Hàn chỉ trích lời xin lỗi của Tổng thống 'quá ngắn'</li>
+                                <li> Điều gì xảy ra nếu Tổng thống Hàn Quốc bị luận tội</li>
+                                <li> Ukraine khoe UAV lai tên lửa có tầm bắn gấp đôi ATACMS</li>
+                                <li> Nhà Trắng trong Giáng sinh cuối nhiệm kỳ của ông Biden</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="group">
+                    <h2>Tin mới</h2>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-vnexpress.vnecdn.net/2024/12/08/70e50c640e85b4dbed94-173362722-7875-6894-1733627962.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=8QiLQEsmj7_eTy4c7QDyKw" style="width: 100%">
+                        </div>
+                        <div class="col-8">
+                            <h3>Tiễn biệt 12 quân nhân hy sinh khi diễn tập</h3>
+                            <p> Sáng 8/12, tại Nhà tang lễ Quốc gia phía Nam (TP HCM), Quân khu 7 tổ chức lễ tang 12 chiến sĩ thuộc tiểu đoàn 17, Sư đoàn 5, hy sinh ngày 2/12 khi diễn tập tác chiến phòng thủ, theo nghi thức quân đội.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-vnexpress.vnecdn.net/2024/12/07/2024-11-28T155914Z-65152929-RC-3844-4890-1733543895.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=Sy8uW4qeKW1RSEnYeepHhA" style="width: 100%">
+                        </div>
+                        <div class="col-8">
+                            <h3>Thế khó của Nga khi phiến quân trỗi dậy ở Syria</h3>
+                            <p> Vì xung đột Ukraine, Nga không thể toàn lực giúp chính quyền Syria chống đỡ phiến quân, nhưng cũng không thể từ bỏ đồng minh chiến lược ở Trung Đông.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-thethao.vnecdn.net/2024/12/08/nottingham-man-utd-1733599382-1839-1733599438.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=HmgIlSR19HG-Ji2CfVPwJQ" style="width: 100%">
+                        </div>
+                        <div class="col-8">
+                            <h3>Man Utd thua trận thứ hai liên tiếp</h3>
+                            <p> Man Utd thua đội khách Nottingham Forest 2-3 ở vòng 15 Ngoại hạng Anh, nhận thất bại thứ hai liên tiếp dưới trướng Ruben Amorim.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu-right">
+            <div class="content">
+                <div class="group">
+                    <h2>Sức khỏe</h2>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-suckhoe.vnecdn.net/2024/12/07/233A3021-1490-1733563884.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=kWfATk4qGV65IteAD1hRdQ" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>31 tỉnh thành phía Nam xây mạng lưới phòng chống ung thư</li>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-suckhoe.vnecdn.net/2024/12/07/z6106536833333-5359fa7d5ef3567-1895-4561-1733562736.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=mhWQRMqvt6QDNPFcTLzouA" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>Bốn học sinh bỏng nặng khi chế pháo nổ</li>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-suckhoe.vnecdn.net/2024/12/07/cong-nhan-nhap-vien-4356-1733544631.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=fs0qeJ58NxOC2ueo7VLdzw" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li> Số công nhân Nghệ An ngộ độc thực phẩm lên 84</li>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="group">
+                    <h2>Kinh Doanh</h2>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-kinhdoanh.vnecdn.net/2024/12/07/goldmansachs-1733588527-3152-1733588589.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=oMaBQ3dcNnURx_gXaFfEqQ" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>Ngân hàng lớn nhất thế giới rút khỏi liên minh Net Zero</li>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-kinhdoanh.vnecdn.net/2024/12/07/etaxmobile16488976779991597221-6467-7893-1733548138.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=DJ0PDritavrHcyrw4_vnuQ" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>Đề xuất cá nhân nợ thuế 10 triệu bị tạm hoãn xuất cảnh</li>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-kinhdoanh.vnecdn.net/2024/12/07/img386617335369637641982447865-7381-5205-1733546069.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=JOsdBTuk5MFoLffg07MYNw" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>Bộ trưởng Kế hoạch và Đầu tư: Tăng trưởng cả năm có thể vượt 7%</li>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="group">
+                    <h2>Giải Trí</h2>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-giaitri.vnecdn.net/2024/12/07/BiKhnhLinh3-1733530961-2310-1733531922.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=46LunLknZpePbc0VBEneow" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>Bùi Khánh Linh đoạt á hậu 3 Miss Intercontinental</li>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-giaitri.vnecdn.net/2024/12/06/ca-si-dan-reynolds-1733502171-2392-1733502214.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=vVZt213-Jh2zQGwjslBkpA" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>Imagine Dragons khuấy động sân khấu Hà Nội</li>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://i1-giaitri.vnecdn.net/2024/12/07/john-lennon-1-1733545363-4601-1733545381.png?w=220&h=132&q=100&dpr=1&fit=crop&s=VhNakVYGotEobshuql0nZA" style="width: 80%">
+                        </div>
+                        <div class="col-8">
+                            <li>Khắc Triệu hát ở đêm nhạc tưởng nhớ John Lennon</li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer>
+    <div class="footer-content">
+        <p>© 64KTPM3   Nguyễn Việt Dũng -  Đinh Quang Đạt - Đỗ Văn Đạt</p>
+    </div>
+</footer>
+</html>
